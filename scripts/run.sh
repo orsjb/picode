983d1fa543dd7be98979e7fce9fdd7cd7775128a
orsjb <ollie@icarus.nu>
2014-04-01 13:36:35 +1100

    Updated run script. The run script now attempts to rename the pi's hostname to "pisound-<MAC>" where MAC is the system's wifi MAC address with the colons removed. e.g., "pisound-009e959c4dbc". If the hostname is not already set to that, it sets /etc/hostname and reboots.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# get the MAC address to use as hostname
+NEWHOST=`cat /sys/class/net/wlan0/address | sed s/://g`
+OLDHOST=`cat /etc/hostname`
+
+NEWHOST=pisound-${NEWHOST}
+
+if [ "$NEWHOST" != "$OLDHOST" ] 
+then
+	echo $NEWHOST > hostname
+	sudo mv hostname /etc/
+	sudo reboot
+fi
+
 DIR=`dirname $0`
 cd ${DIR}/..
 /usr/bin/java -cp build/picode.jar dynamic.DynamoPI > stdout
