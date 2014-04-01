81a64d93c54a0812eb69d927141c97ea876622a1
orsjb <ollie@icarus.nu>
2014-04-01 19:27:29 +1100

    Success with Synchronizer. Refined version now with a long-term time adjustment for greater stability.

--- src/synch/Synchronizer.java
+++ src/synch/Synchronizer.java
@@ -34,11 +34,14 @@ public class Synchronizer {
 	MulticastSocket broadcastSocket;
 	String multicastGroup = "225.2.2.5";
 	int multicastPort = 2225;
-	long timeCorrection;			//add this to current time to get the REAL current time
+	long timeCorrection = 0;			//add this to current time to get the REAL current time
+	long stableTimeCorrection = 0;
+	int stabilityCount = 0;
 
 	boolean on = true;
 	boolean verbose = false;
 	boolean veryverbose = false;
+	boolean timedebug = false;
 	
 	Map<Long, Map<String, long[]>> log;		//first referenced by message send time, then by respodent's name, with the time the respondent replied and the current time
 	
@@ -60,24 +63,23 @@ public class Synchronizer {
 		}
 	}
 	
-	public long localTimeNow() {
-		return System.currentTimeMillis();
+	public long stableTimeNow() {
+		return System.currentTimeMillis() + stableTimeCorrection;
 	}
 	
 	public long correctedTimeNow() {
-		return localTimeNow() + timeCorrection;
+		return stableTimeNow() + timeCorrection;
 	}
 	
 	public void displayClock() {
 		Thread t = new Thread() {
 			public void run() {
 				while(on) {
-					long sleepTime = 0;
 					long timeNow = correctedTimeNow();
 					if(timeNow % 10000 < 4) {
 						//display
 						Date d = new Date(timeNow);
-						System.out.println("The time is: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+						System.out.println("The time is: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + " (fluctuation = " + timeCorrection + ")");
 					}
 					try {
 						Thread.sleep(3);
@@ -118,7 +120,7 @@ public class Synchronizer {
 		Thread t = new Thread() {
 			public void run() {
 				while(on) {
-					broadcast("s " + myMAC + " " + localTimeNow() + " " + myMAC + " " + localTimeNow());	
+					broadcast("s " + myMAC + " " + stableTimeNow() + " " + myMAC + " " + stableTimeNow());	
 					//the last two components are just to ensure that send and return messages are same length to avoid network delays
 					try {
 						Thread.sleep(500 + (int)(100 * Math.random()));	//randomise send time to break network send patterns
@@ -144,15 +146,25 @@ public class Synchronizer {
 	}
 	
 	private void calculateTimeCorrection() {
+		
 		for(Long sendTime : log.keySet()) {
 			Map<String, long[]> responses = log.get(sendTime);
 			//find the leader
 			String theLeader = myMAC;
+			
+			if(timedebug) System.out.println("At send time = " + sendTime);
+			
 			for(String mac : responses.keySet()) {
+				
+				if(timedebug) System.out.println("          Response from: " + mac + " return sent: " + responses.get(mac)[0] + ", received: " + responses.get(mac)[1]);
+				
 				if(theLeader.compareTo(mac) < 0) {
 					theLeader = mac;
 				}
 			}	
+			
+			if(timedebug) System.out.println("Leader is " + theLeader);
+			
 			if(theLeader != myMAC) {
 				//if you are not the leader then make a time adjustment
 				long[] times = responses.get(theLeader);
@@ -161,11 +173,18 @@ public class Synchronizer {
 				long roundTripTime = receiveTime - sendTime;
 				long messageTime = roundTripTime / 2;
 				long receiveTimeAccordingToLeader = leaderResponseTime + messageTime;
-				timeCorrection = receiveTime - receiveTimeAccordingToLeader;
+				timeCorrection = receiveTimeAccordingToLeader - receiveTime;
+				if(timedebug) System.out.println("time correction: " + timeCorrection + ", message time: " + messageTime + ", response sent: " + leaderResponseTime + ", response received: " + receiveTime);
 			}
 		}		
 		//finally, clear the log (for now - we might make the log last longer later)
 		log.clear();
+		//stability count
+		if(stabilityCount++ == 20) {
+			stabilityCount = 0;
+			stableTimeCorrection = timeCorrection;
+			timeCorrection = 0;
+		}
 	}
 	
 	private void getMyMACAddressAndIPAddress() throws SocketException {
@@ -184,14 +203,14 @@ public class Synchronizer {
 	        }
 	      }
 	      if(lStringBuffer.length() > 0) {
-	    	  if(verbose) System.out.print("Interface: " + nif.getDisplayName());
-	    	  if(verbose) System.out.println(" (MAC=" + lStringBuffer + ")");
+	    	  System.out.print("Interface: " + nif.getDisplayName());
+	    	  System.out.println(" (MAC=" + lStringBuffer + ")");
 	    	  macs.add(lStringBuffer.toString());
 		      Enumeration<InetAddress> inetAddresses = nif.getInetAddresses();
 		      for (InetAddress inetAddress : Collections.list(inetAddresses)) {
 		    	  if(inetAddress instanceof Inet4Address) {
 		    		  ips.add(inetAddress.getHostAddress());
-		    		  if(verbose) System.out.println(" -- InetAddress: " + inetAddress.getHostAddress());
+		    		  System.out.println(" -- InetAddress: " + inetAddress.getHostAddress());
 		    	  }
 		    	  
 		      }
@@ -202,7 +221,7 @@ public class Synchronizer {
 		//but it doesn't really matter because this is just to identify the unit
 		myMAC = macs.get(0);
 		myIP = ips.get(0);
-		if(verbose) System.out.println("My IP address: " + myIP);
+		System.out.println("My IP address: " + myIP);
 	}
 	
 	public void messageReceived(String msg) {
@@ -214,7 +233,7 @@ public class Synchronizer {
 			//an original send message
 			//respond if you were not the sender
 			if(!parts[1].equals(myMAC)) {
-				broadcast("r " + parts[1] + " " + parts[2] + " " + myMAC + " " + localTimeNow());
+				broadcast("r " + parts[1] + " " + parts[2] + " " + myMAC + " " + stableTimeNow());
 			}
 		} else if(parts[0].equals("r")) {
 			//a response message
@@ -224,13 +243,14 @@ public class Synchronizer {
 				long timeOriginallySent = Long.parseLong(parts[2]);
 				String otherMAC = parts[3];
 				long timeReturnSent = Long.parseLong(parts[4]);
-				long currentTime = localTimeNow();
-				long returnTripTime = currentTime - timeOriginallySent;
-				long timeAheadOfOther = (currentTime - (returnTripTime / 2)) - timeReturnSent;	//+ve if this unit is ahead of other unit
+				long currentTime = stableTimeNow();
 				log(timeOriginallySent, otherMAC, timeReturnSent, currentTime);
-				if(verbose) System.out.println("Return trip from " + myMAC + " to " + parts[3] + " took " + returnTripTime + "ms");
-				if(verbose) System.out.println("This machine (" + myMAC + ") is " + (timeAheadOfOther > 0 ? "ahead of" : "behind") + " " + otherMAC + " by " + Math.abs(timeAheadOfOther) + "ms");
-				
+				if(verbose) {
+					long returnTripTime = currentTime - timeOriginallySent;
+					long timeAheadOfOther = (currentTime - (returnTripTime / 2)) - timeReturnSent;	//+ve if this unit is ahead of other unit
+					System.out.println("Return trip from " + myMAC + " to " + parts[3] + " took " + returnTripTime + "ms");
+					System.out.println("This machine (" + myMAC + ") is " + (timeAheadOfOther > 0 ? "ahead of" : "behind") + " " + otherMAC + " by " + Math.abs(timeAheadOfOther) + "ms");
+				}
 			}
 		}
 	}
