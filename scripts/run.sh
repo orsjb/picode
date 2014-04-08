9c2ab2d4d51109a3acb910a66650f8601ca091e1
orsjb <ollie@icarus.nu>
2014-04-08 17:11:54 +1000

    Tests.

--- scripts/run.sh
+++ scripts/run.sh
@@ -21,7 +21,7 @@ then
 	sudo reboot 
 fi
 
-# move to the correct dir for running java
+# move to the correct dir for running java (one level above where this script is)
 
 DIR=`dirname $0`
 cd ${DIR}/..
@@ -34,7 +34,11 @@ SR=11000
 INS=0
 OUTS=1
 
+# libs/minimulib/minimu9-ahrs -b /dev/i2c-1 | /usr/bin/sudo /usr/bin/java -cp build/picode.jar test.PrintStdIn $BUF $SR $INS $OUTS > stdout &
+
+# various old or test scripts
+
+echo ~ > stdout &
+# libs/minimulib/minimu9-ahrs -b /dev/i2c-1 > stdout &
 # /usr/bin/sudo /usr/bin/java -cp build/picode.jar dynamic.DynamoPI $BUF $SR $INS $OUTS > stdout &
 # /usr/bin/sudo /usr/bin/java -cp build/picode.jar synch.Synchronizer $BUF $SR $INS $OUTS > stdout &
-# libs/minimulib/minimu9-ahrs -b /dev/i2c-1 | /usr/bin/sudo /usr/bin/java -cp build/picode.jar test.PrintStdIn $BUF $SR $INS $OUTS > stdout &
-libs/minimulib/minimu9-ahrs -b /dev/i2c-1 > stdout &
