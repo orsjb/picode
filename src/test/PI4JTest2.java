a6c34be5c231f39c94a007ea1f0fcc94cc4fab8d
orsjb <ollie@icarus.nu>
2014-04-09 23:11:48 +1000

    Reporting.

--- src/test/PI4JTest2.java
+++ src/test/PI4JTest2.java
@@ -20,8 +20,11 @@ public class PI4JTest2 {
 
 	public static void main(String[] args) {
 		final GpioController gpio = GpioFactory.getInstance();
+		System.out.println("Got GPIO instance.");
 		GpioPinDigitalInput myInPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03); 
+		System.out.println("Got GPIO pin 3.");
 		myInPin.addListener(new MyInputListener());
+		System.out.println("Set up listener for GPIO pin 3.");
 		
 		while(true) {}
 	}
