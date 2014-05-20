5597b93f1494ffcba9f5fc019824cf4a483f33c1
orsjb <ollie@icarus.nu>
2014-05-20 10:25:36 +1000

    Something has gone horribly wrong with audio. It's broken all of a sudden for no obvious reason. JavaSound failing to find the mixer.

--- src/core/AudioSetup.java
+++ src/core/AudioSetup.java
@@ -6,14 +6,17 @@ import net.beadsproject.beads.core.io.JavaSoundAudioIO;
 
 public abstract class AudioSetup { 
 
-	public static AudioContext getAudioContext(String[] args) {			//args are bufSize (8192), sample rate (22050), input channels (0), output channels (1)
+	public static AudioContext getAudioContext(String[] args) {			//args are bufSize (512), sample rate (11000), input channels (0), output channels (1)
 		try {
 			int bufSize = Integer.parseInt(args[0]);
 			int sampleRate = Integer.parseInt(args[1]);
 			int inchans = Integer.parseInt(args[2]);
 			int outchans = Integer.parseInt(args[3]);
 			System.out.println("Creating AudioContext with args: bufSize=" + bufSize + ", sampleRate=" + sampleRate + ", ins=" + inchans + ", outs=" + outchans);
-			return new AudioContext(new JavaSoundAudioIO(bufSize), bufSize, new IOAudioFormat(sampleRate, 16, inchans, outchans));
+			JavaSoundAudioIO.printMixerInfo();
+			JavaSoundAudioIO jsaio = new JavaSoundAudioIO(bufSize);
+			AudioContext ac = new AudioContext(jsaio, bufSize, new IOAudioFormat(sampleRate, 16, inchans, outchans));
+			return ac;
 		} catch(Exception e) {
 			System.out.println("Warning, correct args have not been supplied to AudioSetup.getAudioContext()");
 			return getAudioContext();
@@ -21,6 +24,6 @@ public abstract class AudioSetup {
 	}
 	
 	public static AudioContext getAudioContext() {			
-		return getAudioContext(new String[] {"8192", "22050", "0", "1"});
+		return getAudioContext(new String[] {"512", "11000", "0", "1"});
 	}
 }
