34bf757e9db8c24d6ab6f0c7fa69bfaf054811fe
orsjb <ollie@icarus.nu>
2014-05-22 16:59:50 +1000

    x

--- src/core/Synchronizer.java
+++ src/core/Synchronizer.java
@@ -32,7 +32,7 @@ public class Synchronizer {
 
 	boolean on = true;
 	boolean verbose = true;
-	boolean veryverbose = false;
+	boolean veryverbose = true;
 	boolean timedebug = false;
 	
 	Map<Long, Map<String, long[]>> log;		//first referenced by message send time, then by respodent's name, with the time the respondent replied and the current time
