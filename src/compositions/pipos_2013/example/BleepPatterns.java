ebbcaf54976291ae3d1ec93fc89435c32fc7a809
orsjb <ollie@icarus.nu>
2014-09-15 13:48:02 +1000

    Recovered from broken code.

--- src/compositions/pipos_2013/example/BleepPatterns.java
+++ src/compositions/pipos_2013/example/BleepPatterns.java
@@ -12,27 +12,34 @@ import core.PIPO;
 public class BleepPatterns implements PIPO {
 
 	public static void main(String[] args) throws Exception {
+		System.out.println("x");
+		
 		String fullClassName = Thread.currentThread().getStackTrace()[1].getClassName().replace(".", "/");
-		SendToPI.send(fullClassName, new String[] {"pi9"});
+		SendToPI.send(fullClassName, new String[]{
+				
+				"pisound-009e959c5093.local", 
+				"pisound-009e959c47ef.local", 
+				"pisound-009e959c4dbc.local", 
+				"pisound-009e959c3fb2.local",
+				"pisound-009e959c50e2.local",
+				"pisound-009e959c47e8.local",
+				"pisound-009e959c510a.local",
+				"pisound-009e959c502d.local",
+				
+				});
 	}
 	
 	@Override
 	public void action(final DynamoPI d) {
 
-		
 		d.reset();
-		
 		int id = d.myIndex();
 		
-		
 		final Glide g = new Glide(d.ac);
-
 		WavePlayer wp = new WavePlayer(d.ac, g, Buffer.SINE);
 		d.pl.addInput(wp);
 		
-		
 		MiniMU.MiniMUListener myListener = new MiniMU.MiniMUListener() {
-
 			@Override
 			public void accelData(double x, double y, double z) {
 				g.setValue((float)x);
@@ -41,7 +48,6 @@ public class BleepPatterns implements PIPO {
 			public void freefallEvent() {
 				//
 			}
-			
 		};
 		d.mu.addListener(myListener);
 		
