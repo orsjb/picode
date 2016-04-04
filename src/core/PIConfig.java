f7eef60b7c7dc4d955b6ee82146ba12691da14be
Sam Gillespie <samgwise@gmail.com>
2016-04-04 12:42:12 +1000

    Sketched basic structure for new config objects.

---
+++ src/core/PIConfig.java
@@ -0,0 +1,17 @@
+package core;
+
+public class PIConfig implements EnvironmentConf {
+
+	@Override
+	public String getControllerHostname() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMyHostName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
