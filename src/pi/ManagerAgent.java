67d156c70290bbffb4db7ee11293db9100bfd1a3
Sam Gillespie <samgwise@gmail.com>
2016-04-07 21:11:31 +1000

    Added additional diag info to pi.MangerAgent

--- src/pi/ManagerAgent.java
+++ src/pi/ManagerAgent.java
@@ -5,8 +5,8 @@ import core.PIConfig;
 public class ManagerAgent {
 
 	public static void main(String[] args) {
-		System.out.println("PI Manager Agent Started");
 		PIConfig env = new PIConfig();
+		System.out.println("PI Manager Agent Started as: " + env.getMyHostName());
 		System.out.println("Listening for Controller...");
 		System.out.println("Found controller on host: " + env.getControllerHostname());
 		
