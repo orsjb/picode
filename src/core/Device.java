512b08c997db7bcb43a4167165069408cbd6a546
Sam Gillespie <samgwise@gmail.com>
2016-04-07 13:31:15 +1000

    Added Windows specific code for selecting network interfaces.

--- src/core/Device.java
+++ src/core/Device.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public abstract class Device {
@@ -16,13 +17,41 @@ public abstract class Device {
 		String tmpMAC = null;
 		try {
 			NetworkInterface netInterface;
+			System.out.println("Detected OS: " + System.getProperty("os.name"));
 			if (System.getProperty("os.name").startsWith("Mac OS")) {
 				netInterface = NetworkInterface.getByName("en1");
 				//if you can't get the wlan then get the ethernet mac address:
 				if(netInterface == null) {
 					netInterface = NetworkInterface.getByName("en0");
 				}
-			} else {
+			}
+			else if (System.getProperty("os.name").startsWith("Windows")) {
+				System.out.println("Interfaces:");
+				Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+				String favouriteInterfaceName = null;
+				while (interfaces.hasMoreElements()) {
+					netInterface = interfaces.nextElement();
+					
+					//lets at least try and get a real interface...
+					if (netInterface.supportsMulticast() && !netInterface.isLoopback() && netInterface.isUp() && !netInterface.isVirtual()) {
+						favouriteInterfaceName = netInterface.getName();
+						System.out.println("I like: " + favouriteInterfaceName + ", " + netInterface.getDisplayName());
+					}
+					else {
+						System.out.println("Ignored: " + netInterface.getName() + ", " + netInterface.getDisplayName());
+					}
+				}
+				
+				if (favouriteInterfaceName != null ) {
+					netInterface = NetworkInterface.getByName(favouriteInterfaceName);
+				}
+				else {
+					netInterface = NetworkInterface.getByName("wlan0"); // feeling lucky punk?
+				}
+				
+				System.out.println("Selected interface: " + netInterface.getDisplayName());
+			}
+			else {
 				netInterface = NetworkInterface.getByName("wlan0");
 				if (netInterface == null) {
 					netInterface = NetworkInterface.getByName("eth0");
@@ -65,7 +94,14 @@ public abstract class Device {
 			
 			//If everything still isn't working lets try via our interface for an IP address
 			if (tmpHostname == null) {
-				tmpHostname = netInterface.getInetAddresses().nextElement().getHostAddress().split("%")[0];
+				String address = netInterface.getInetAddresses().nextElement().getHostAddress();
+				//strip off trailing interface name if present
+				if (address.contains("%")) {
+					tmpHostname = address.split("%")[0];
+				}
+				else {
+					tmpHostname = address;
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
